58872e96276830800787ab84c723e56d3b7278fa
Nich <nctrenco@gmail.com>
2019-09-18 18:11:19 +0000

    cryptfshw: Implement QTI Cryptfs_hw 1.0 HAL
    Based on the pre-Q libcryptfs_hw.
    See https://github.com/LineageOS/android_vendor_qcom_opensource_cryptfs_hw/commit/6b99d61baaee529c181a663a5f205b66c91141ff
    for the commit that moved the ioctl calls to the HIDL HAL.
    
    Change-Id: I9299c1bc65bb64240047983f6b9123c0388721bf

--- cryptfshw/1.0/CryptfsHw.h
+++ cryptfshw/1.0/CryptfsHw.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The LineageOS Project
+ * Copyright (C) 2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,19 +35,38 @@ using ::android::hardware::Return;
 using ::android::hardware::Void;
 using ::android::sp;
 
+enum cryptfs_hw_key_management_usage_type {
+    CRYPTFS_HW_KM_USAGE_DISK_ENCRYPTION			= 0x01,
+    CRYPTFS_HW_KM_USAGE_FILE_ENCRYPTION			= 0x02,
+    CRYPTFS_HW_KM_USAGE_UFS_ICE_DISK_ENCRYPTION		= 0x03,
+    CRYPTFS_HW_KM_USAGE_SDCC_ICE_DISK_ENCRYPTION	= 0x04,
+    CRYPTFS_HW_KM_USAGE_MAX
+};
+
 struct CryptfsHw : public ICryptfsHw {
+public:
     // Methods from ::vendor::qti::hardware::cryptfshw::V1_0::ICryptfsHw follow.
     Return<int32_t> setIceParam(uint32_t flag) override;
     Return<int32_t> setKey(const hidl_string& passwd, const hidl_string& enc_mode) override;
-    Return<int32_t> updateKey(const hidl_string& oldpw, const hidl_string& newpw, const hidl_string& enc_mode) override;
+    Return<int32_t> updateKey(const hidl_string& oldpw, const hidl_string& newpw,
+            const hidl_string& enc_mode) override;
     Return<int32_t> clearKey() override;
 
-    // Methods from ::android::hidl::base::V1_0::IBase follow.
-
+private:
+    int cryptfs_hw_create_key(enum cryptfs_hw_key_management_usage_type usage,
+            unsigned char *hash32);
+    int __cryptfs_hw_wipe_clear_key(enum cryptfs_hw_key_management_usage_type usage,
+            int wipe_key_flag);
+    int cryptfs_hw_wipe_key(enum cryptfs_hw_key_management_usage_type usage);
+    int cryptfs_hw_clear_key(enum cryptfs_hw_key_management_usage_type usage);
+    int cryptfs_hw_update_key(enum cryptfs_hw_key_management_usage_type usage,
+            unsigned char *current_hash32, unsigned char *new_hash32);
+    enum cryptfs_hw_key_management_usage_type map_usage(
+            enum cryptfs_hw_key_management_usage_type usage);
+    unsigned char* get_tmp_passwd(const char* passwd);
+    int is_qseecom_up();
+    int set_key(const char* currentpasswd, const char* passwd, const char* enc_mode, int operation);
 };
-
-// FIXME: most likely delete, this is only for passthrough implementations
-// extern "C" ICryptfsHw* HIDL_FETCH_ICryptfsHw(const char* name);
 
 }  // namespace implementation
 }  // namespace V1_0
